f12941fabaf2cb1c17cb6870d7f7f50c70b8732d
ansalstmd@gmail.com <ansalstmd@gmail.com>
2021-07-19 17:26:19 +0900

    C#과 SQL Server로 CRUD 작업

--- student/Conn/DBConn.cs
+++ student/Conn/DBConn.cs
@@ -9,10 +9,10 @@ namespace student.Conn
 {
     class DBConn
     {
-        private static string uid = "TBM"; // DB 접속 아이디
-        private static string password = "dong8844!@#$%^&*()"; // DB 접속 비밀번호
+        private static string uid = ""; // DB 접속 아이디
+        private static string password = "!@#$%^&*()"; // DB 접속 비밀번호
         private static string database = "student";  // 사용할 DB 이름
-        private static string server = "218.234.193.220,1433";  // 서버 주소 -- sql 서버 접속시 사용한 주소
+        private static string server = "";  // 서버 주소 -- sql 서버 접속시 사용한 주소
         private static SqlConnection conn = null;
 
         public static SqlConnection getConn()
